e0f6bf1fb32778a388d5d5e0975210eaeb19b394
nikitaacodes <nikitaa4822@gmail.com>
2025-09-07 23:59:21 +0530

    routes added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,20 @@ import { Interface } from "readline";
 import SeachBox from "./SearchBox";
 
 interface NavbarProps {
-  variant?: "accounts" | "dashboard";
+  variant?: "accounts" | "dashboard" | "invoice" | "mywallet" | "activity";
 }
 export default function Navbar({ variant }: NavbarProps) {
   return (
     <div className="w-full mx-4 py-1 flex flex-row justify-items-start gap-5">
-      <div className="text-xl font-bold text-blue-950"> Xpenso {variant && <span className="ml-2 capitalize"> {variant}</span>} </div>
+      <div className="text-xl font-bold text-blue-950">
+        {" "}
+        Xpenso {variant && (
+          <span className="ml-2 capitalize"> {variant}</span>
+        )}{" "}
+      </div>
       <div>
         <SeachBox />
       </div>
-     
     </div>
   );
 }
